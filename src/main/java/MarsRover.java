7ed1f7d4c1985e24e2258301e82ecbde68728413
Mathias Sjöblom <mathias.sjoblom@ha.ax>
2022-03-08 23:24:02 +0200

    projekt uppladdning

---
+++ src/main/java/MarsRover.java
@@ -0,0 +1,11 @@
+public class MarsRover {
+    public int x;
+    public int y;
+    public String direction;
+
+    public MarsRover(int x, int y, String direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+}
